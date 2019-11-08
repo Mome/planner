c37d280d95285306d076bf066ae0ec873f1d5ceb
Moritz Meier <moritz.meier@uni-bremen.de>
2019-11-08 14:06:18 +0100

    project parser workds now

--- project_parser.py
+++ project_parser.py
@@ -5,8 +5,10 @@ def to_dot(tree):
     g.node_attr['shape'] = 'box'
     g.node_attr['style'] = 'rounded'
     g.graph_attr['rankdir'] = 'BT'
-    leafs = set()
-    nonleafs = set()
+    g.graph_attr['splines'] = 'ortho'
+    g.edge_attr['arrowhead'] = 'none'
+    #leafs = set()
+    #nonleafs = set()
     def draw_subtree(tree):
         for node in tree:
             if type(node) == str:
@@ -15,17 +17,17 @@ def to_dot(tree):
                 continue
             for node in draw_subtree(node):
                 g.edge(node, last_node)
-                leafs.add(node)
-                nonleafs.add(last_node)
+                #leafs.add(node)
+                #nonleafs.add(last_node)
     with g.subgraph() as s:
         s.attr(rank='same')
         for node in draw_subtree(tree):
             s.node(node)
-    leafs -= nonleafs
-    with g.subgraph() as s:
-        s.attr(rank='same')
-        for leaf in leafs:
-            s.node(leaf)
+    #leafs -= nonleafs
+    #with g.subgraph() as s:
+    #    s.attr(rank='same')
+    #    for leaf in leafs:
+    #        s.node(leaf)
     return g
 
 
@@ -59,8 +61,30 @@ def parse_tree(arg):
 if __name__ == '__main__':
     import sys
     fname = sys.argv[1]
+    if len(sys.argv) >= 3:
+        subproject = sys.argv[2]
+    else:
+        subproject = None
     with open(fname) as f:
         code = f.read()
     tree = parse_tree(code)
+    if subproject:
+        it_tree = iter(tree)
+        projects = []
+        for node in it_tree:
+            if node == subproject:
+                try:
+                    subtree = next(it_tree)
+                except StopIteration:
+                    tree = [node]
+                else:
+                    tree = [node, subtree]
+                break
+            elif type(node) == str:
+                projects.append(node)
+        else:
+            print(f'No subproject {subproject}.', file=sys.stderr)
+            print('Choose from:', *projects, file=sys.stderr)
+            sys.exit(1)
     g = to_dot(tree)
     print(g.source)
